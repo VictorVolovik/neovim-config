187a32f9ea2b45c7aa4693d9fed4801d1c080a7d
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-01-04 17:51:29 +0400

    Setup autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,56 +1,62 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Diagnostics
-      vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-      vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist)
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-      -- Defitions, declarations and types
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-      vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, {})
-      vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<Leader>gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "<Leader>gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references, {})
+			-- Diagnostics
+			vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist)
 
-      -- Workspace
-      vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, {})
-      vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, {})
-      vim.keymap.set("n", "<Leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, {})
+			-- Defitions, declarations and types
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<Leader>gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "<Leader>gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references, {})
 
-      -- Code actions & refactoring
-      vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ async = true })
-      end, {})
-    end,
-  },
+			-- Workspace
+			vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, {})
+			vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, {})
+			vim.keymap.set("n", "<Leader>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, {})
+
+			-- Code actions & refactoring
+			vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<Leader>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, {})
+		end,
+	},
 }
