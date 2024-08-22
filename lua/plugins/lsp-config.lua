be1d46daf23b787cdeb62ec1f7751cd5b044eedd
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-08-22 07:21:15 +0400

    Add border to lsp handlers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,6 +33,18 @@ return {
         capabilities = capabilities,
       })
 
+      -- Border
+      local _border = "single"
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = _border,
+      })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = _border,
+      })
+      vim.diagnostic.config({
+        float = { border = _border },
+      })
+
       -- Diagnostics
       vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float)
       vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
