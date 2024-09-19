e75e90fbcb74b2c7db2ebbe27ed906ed5a6be752
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-09-19 09:23:55 +0400

    Fix typescript lsp & update deps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "tsserver",
+          "ts_ls",
           "eslint",
           "jsonls",
           "emmet_language_server",
@@ -28,7 +28,7 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities,
       })
       lspconfig.emmet_language_server.setup({
