4eb53cf3b292c99f395e022d8864c28a16d55bfe
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-12-30 11:10:11 +0400

    Turn off italic style for keywords

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,7 +9,7 @@ return {
       undercurl = true, -- enable undercurls
       commentStyle = { italic = true },
       functionStyle = {},
-      keywordStyle = { italic = true },
+      keywordStyle = { italic = false },
       statementStyle = { bold = true },
       typeStyle = {},
       transparent = false, -- do not set background color
