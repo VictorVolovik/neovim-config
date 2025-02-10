0795e3c413c91396880fd48145d6b2637171de55
Victor Volovik <VictorVolovik@users.noreply.github.com>
2025-02-10 13:34:34 +0400

    Add staticcheck support & update deps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -25,6 +25,15 @@ return {
         null_ls.builtins.diagnostics.sqlfluff.with({
           extra_args = { "--dialect", "postgres" },
         }),
+        null_ls.builtins.diagnostics.staticcheck.with({
+          diagnostics_postprocess = function(diagnostic)
+            if diagnostic.end_lnum == 0 or diagnostic.end_col == 0 then
+              diagnostic.end_lnum = diagnostic.lnum
+              diagnostic.end_col = diagnostic.col + 120
+              diagnostic.severity = vim.diagnostic.severity.WARN
+            end
+          end,
+        }),
       },
     })
   end,
