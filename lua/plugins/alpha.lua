ffd0f6fb061a59ae7fb12589e86b5a7a0984fc61
Victor Volovik <VictorVolovik@users.noreply.github.com>
2026-04-12 15:59:19 +0400

    Fix dev icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.startify")
+		dashboard.file_icons.provider = "devicons"
 		alpha.setup(dashboard.opts)
 	end,
 }
